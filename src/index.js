ed8660648143aad849db135739f8e108e7fce62f
leekyung <azaz04210421@gmail.com>
2025-04-29 00:17:20 +0900

    Change BrowserRouter to HashRouter to fix routing issues in production

--- src/index.js
+++ src/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/LocalAuthContext';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
-import UpgradeVIP from './components/Auth/UpgradeVIP';
 import Logout from './components/Auth/Logout';
-import Admin from './pages/Admin';
 import AdminDashboard from './pages/AdminDashboard';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+// 라우팅 복원
 root.render(
   <React.StrictMode>
     <AuthProvider>
@@ -22,9 +21,7 @@ root.render(
           <Route path="/" element={<App />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/upgrade-vip" element={<UpgradeVIP />} />
           <Route path="/logout" element={<Logout />} />
-          <Route path="/admin" element={<Admin />} />
           <Route path="/admin-dashboard" element={<AdminDashboard />} />
         </Routes>
       </Router>
